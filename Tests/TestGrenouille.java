73dabd25db30ef4b5f5d42481e97b4532fc6ba61
Hoyet <Alexandre.Hoyet@univ-brest.fr>
2014-02-03 11:42:05 +0100

    test unit grenouille

---
+++ Tests/TestGrenouille.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestGrenouille {
+	@Before
+	public void initialisationTest() {
+		// la list
+		// deux positions
+
+	}
+
+	@Test
+	private void testPeutAvancer() {
+		//  TODO a completer
+
+	}
+
+}
